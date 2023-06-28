b7de50b692dea286b17180208057a8b9897551b6
Jaima07 <jaima.jarnaz@bjitgroup.com>
2023-06-28 23:46:25 +0600

    user register implemented

--- pages/auth/signup/index.tsx
+++ pages/auth/signup/index.tsx
@@ -4,48 +4,91 @@ import Button from "@/components/atoms/button";
 import { Note } from "@/components/atoms/note/index.";
 import Container from "@/components/atoms/container";
 import Heading from "@/components/atoms/heading";
+import { FORM_DATA_TYPES } from "helpers/types";
 import { useState } from "react";
 
 const SignUp = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState<any>();
   const [message, setMessage] = useState("");
+
+  const initialState = {
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const [formData, setFormData] = useState<FORM_DATA_TYPES>(initialState);
+  const [validationErrors, setValidationErrors] =
+    useState<FORM_DATA_TYPES>(initialState);
+  const [error, setError] = useState(false);
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const formData = {
-      name,
-      email,
-      phone,
-      password,
-      confirmPassword,
-    };
-    //Create new user registration
-    try {
-      const jsonData = JSON.stringify(formData);
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: jsonData,
-      };
-      const user = await fetch(
-        `${process.env.NEXT_PUBLIC_USER_CREATE_API}`,
-        options
-      );
-      const result = await user.json();
-      setMessage(result.message);
-    } catch (error) {
-      console.log(error);
+
+    //perform validation errors
+    let errors: any = {};
+
+    if (formData.name.trim() === "") {
+      errors.name = "Name is required";
+    }
+    if (formData.email.trim() === "") {
+      errors.email = "Email is required";
+    }
+    if (formData.phone.trim() === "") {
+      errors.phone = "Phone number is required";
+    }
+    if (formData.password.trim() === "") {
+      errors.password = "Password is required";
+    }
+
+    // Checking errors length
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+    } else {
+      //Create new user registration logic
+      try {
+        console.log(formData);
+        const jsonData = JSON.stringify(formData);
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: jsonData,
+        };
+        const user = await fetch(
+          `${process.env.NEXT_PUBLIC_USER_CREATE_API}`,
+          options
+        );
+        const result = await user.json();
+        setMessage(result.message);
+
+        if (result.success === true) {
+          setError(false);
+
+          // Reset the form state
+          setFormData(initialState);
+          setValidationErrors(initialState);
+        } else {
+          setError(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
   return (
     <Container width="400" margin="middle" type="withShadow" padding="30">
-      {message ? <Note color="green">{message}</Note> : ""}
+      {message ? <Note color={error ? "danger" : "green"}>{message}</Note> : ""}
 
       <Container>
         <Heading tag="h4">Sign Up</Heading>
@@ -54,38 +97,53 @@ const SignUp = () => {
         <CustomInput
           type="text"
           label="Name"
-          handleChange={(e: any) => {
-            setName(e.target.value);
-          }}
+          name="name"
+          handleChange={handleInputChange}
+          value={formData.name}
         />
+        {validationErrors.name && (
+          <Note color="danger">{validationErrors.name}</Note>
+        )}
         <CustomInput
           type="email"
           label="Email"
-          handleChange={(e: any) => {
-            setEmail(e.target.value);
-          }}
+          name="email"
+          handleChange={handleInputChange}
+          value={formData.email}
         />
+        {validationErrors.email && (
+          <Note color="danger">{validationErrors.email}</Note>
+        )}
         <CustomInput
           type="number"
           label="Phone"
-          handleChange={(e: any) => {
-            setPhone(e.target.value);
-          }}
+          name="phone"
+          handleChange={handleInputChange}
+          value={formData.phone}
         />
+        {validationErrors.phone && (
+          <Note color="danger">{validationErrors.phone}</Note>
+        )}
         <CustomInput
           type="password"
           label="Password"
-          handleChange={(e: any) => {
-            setPassword(e.target.value);
-          }}
+          name="password"
+          handleChange={handleInputChange}
+          value={formData.password}
         />
+        {validationErrors.password && (
+          <Note color="danger">{validationErrors.password}</Note>
+        )}
         <CustomInput
           type="password"
           label="Confirm Password"
-          handleChange={(e: any) => {
-            setConfirmPassword(e.target.value);
-          }}
+          name="confirmPassword"
+          handleChange={handleInputChange}
+          value={formData.confirmPassword}
         />
+        {validationErrors.confirmPassword && (
+          <Note color="danger">{validationErrors.confirmPassword}</Note>
+        )}
         <Button>SUBMIT</Button>
       </form>
     </Container>
